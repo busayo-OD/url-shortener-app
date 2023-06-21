be7ec13485123c01b3ce4ea5f9ca6130e21e10ab
Busayo Dada <toyinoluwabusayo@gmail.com>
2023-06-21 18:24:42 +0100

    qrcode

--- backend/src/controller/qrcode.controller.js
+++ backend/src/controller/qrcode.controller.js
@@ -2,15 +2,22 @@ const qr = require('qrcode');
 const Url = require('../model/url.model');
 
 const generateQrCode = async (req, res, next) => {
+    const { id } = req.params;
     try {
-        const { shortUrl } = req.body;
-        const qrCode = await qr.toDataURL(shortUrl);
-        const url = await Url.findOne({ shortUrl: req.body.shortUrl });
+        const url = await Url.findById(id);
+  
+        if (!url) {
+            console.log("Url not found");
+            return res.status(404).send({ error: 'Url not found' });
+        }
+
+        const genUrl = url.shortUrl;
+        const qrCode = await qr.toDataURL(genUrl);
         
         url.qrCode = qrCode;
         await url.save();
-
-        res.send(`<img src="${qrCode}" alt="QR Code">`);
+        return res.status(200).json({QrCode:qrCode});
+        
     } catch (err) {
         next(err);
     }
--- backend/src/routes/qrcode.routes.js
+++ backend/src/routes/qrcode.routes.js
@@ -1,6 +1,7 @@
 const qrCodeRouter = require('express').Router();
 const qrCodeController = require('../controller/qrcode.controller');
+const auth = require('../middleware/auth')
 
-qrCodeRouter.post('/create', qrCodeController.generateQrCode);
+qrCodeRouter.post('/create/:id', auth, qrCodeController.generateQrCode);
 
 module.exports = qrCodeRouter;
