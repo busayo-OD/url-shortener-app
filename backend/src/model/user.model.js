f69a79ecde31a17af51534b9e212612e3929951e
Busayo Dada <toyinoluwabusayo@gmail.com>
2023-07-09 18:11:37 +0100

    custom error message

--- backend/src/model/user.model.js
+++ backend/src/model/user.model.js
@@ -5,8 +5,16 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   first_name: { type: String, required: true },
   last_name: { type: String, required: true },
-  email: { type: String, required: true, unique: true}, 
-  password: { type: String, required: true },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true
+  }, 
+  password: { 
+    type: String, 
+    required: true,
+    minLength: [4, 'Password is too short'] 
+  },
   urls: [
     {
       type: mongoose.Schema.Types.ObjectId,
