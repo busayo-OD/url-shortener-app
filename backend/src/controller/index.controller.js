c4c9df4d41bd6af0d19d3c9000396a5fe891e7ac
Busayo Dada <toyinoluwabusayo@gmail.com>
2023-07-09 21:00:04 +0100

    clicks

--- backend/src/controller/index.controller.js
+++ backend/src/controller/index.controller.js
@@ -1,27 +1,50 @@
 const Url = require('../model/url.model');
+const axios = require('axios');
 
 // redirect to long/original url
 const getLongUrl = async (req, res) => {
-    try {
-        const url = await Url.findOne({backHalf: req.params.customSlug})
+  try {
+    const url = await Url.findOneAndUpdate(
+      { backHalf: req.params.customSlug },
+      { $inc: { clickCount: 1 } },
+      { new: true }
+    );
 
-        if(url) {
-            url.clicks++
-            await url.save();
+    const ip = 
+        req.headers['cf-connecting-ip'] ||
+        req.headers['x-real-ip'] ||
+        req.headers['x-forwarded-for'] ||
+        req.socket.remoteAddress || '';
 
-            const clickSource = req.headers.referer || 'Direct';
-            url.clickSources.push(clickSource);
+    if (url) {
+      const click = {
+        timestamp: new Date(),
+        ipAddress: ip
+      };
 
-            await url.save()
-            return res.redirect(url.longUrl);
-        } else {
-            return res.status(404).json('No url found')
-        }
+      if (!url.clicks) {
+        url.clicks = [];
+      }
 
-    } catch (err) {
-        console.error(err);
-        res.status(500).json('Server error');
+      // Get geolocation information based on IP address
+      const response = await axios.get(`https://ipapi.co/${click.ipAddress}/json/`);
+      if (response.status === 200) {
+        const { city } = response.data;
+        click.city = city;
+      }
+
+      url.clicks.push(click);
+      await url.save();
+
+      return res.redirect(url.longUrl);
+    } else {
+      return res.status(404).json('No URL found');
     }
-}
+  } catch (err) {
+    console.error(err);
+    res.status(500).json('Server error');
+  }
+};
 
+  
 module.exports = {getLongUrl}
--- backend/src/model/url.model.js
+++ backend/src/model/url.model.js
@@ -4,23 +4,34 @@ const Schema = mongoose.Schema;
 const urlSchema = new Schema({
     backHalf: String,
     longUrl: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     shortUrl: String,
     owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
     },
     title: String,
     tags: [String],
-    clickSources: [String],
     qrCode: String,
-    clicks:{
-        type: Number,
-        default: 0
+    clickCount: {
+      type: Number,
+      default: 0,
     },
-    date: {type: String, default: Date.now}
-});
+    clicks: [
+      {
+        timestamp: {
+          type: Date,
+          default: Date.now,
+        },
+        ipAddress: String,
+      },
+    ],
+    date: {
+      type: String,
+      default: Date.now,
+    },
+  });
 
 module.exports = mongoose.model('Url', urlSchema);
